8634fbe6638a503946b63c33e8a2a4146b6a464f
Maya Wright <mayagwright20@hotmail.com>
2025-08-24 21:43:13 +0100

    Add home page flags

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,38 +1,82 @@
+import { IconSymbol } from '@/components/ui/IconSymbol';
+import { COLOURS } from '@/constants/colours';
+import { SHADOW } from '@/constants/styles';
 import useCountries from '@/hooks/useCountries';
 import { countries } from '@/lib/country-codes';
+import { useState } from 'react';
 import {
-  ActivityIndicator,
   Image,
   SafeAreaView,
+  ScrollView,
   StyleSheet,
   Text,
+  TouchableOpacity,
+  View,
 } from 'react-native';
 
 export default function HomeScreen() {
-  const { loading, allCountries } = useCountries();
+  const { allCountries } = useCountries();
+  const [randomInts, setRadomInts] = useState([0, 10, 130, 112]);
+  const [correctAnswerInt, setCorrectAnswerInt] = useState(
+    Math.floor(Math.random() * 4)
+  );
+
+  console.log('correctAnswer', correctAnswerInt);
 
   return (
-    <SafeAreaView style={styles.container}>
-      {loading ? (
-        <ActivityIndicator size="large" />
-      ) : (
-        <>
-          {allCountries && (
-            <>
-              <Image
-                source={{
-                  uri: `https://flagcdn.com/w2560/${
-                    countries[allCountries[0]['name']['common']]
-                  }.png`,
-                }}
-                style={{ width: '100%', height: 200 }}
-              />
-              <Text>{allCountries[0]['name']['common']}</Text>
-            </>
-          )}
-        </>
-      )}
-    </SafeAreaView>
+    <ScrollView contentContainerStyle={styles.scrollview}>
+      <SafeAreaView style={styles.container}>
+        {allCountries && (
+          <View style={styles.imageWrapper}>
+            <IconSymbol
+              style={styles.icon}
+              size={35}
+              name="heart"
+              color={'red'}
+            />
+            <Image
+              source={{
+                uri: `https://flagcdn.com/w2560/${
+                  countries[
+                    allCountries[randomInts[correctAnswerInt]]['name']['common']
+                  ]
+                }.png`,
+              }}
+              style={styles.image}
+            />
+          </View>
+        )}
+        <View style={styles.itemsWrapper}>
+          {allCountries &&
+            randomInts.map((item, index) => {
+              if (index === correctAnswerInt) {
+                return (
+                  <TouchableOpacity key={index} style={styles.item}>
+                    <Text style={styles.title}>
+                      {
+                        allCountries[randomInts[correctAnswerInt]]['name'][
+                          'common'
+                        ]
+                      }
+                    </Text>
+                  </TouchableOpacity>
+                );
+              } else {
+                return (
+                  <TouchableOpacity style={styles.item} key={index}>
+                    <Text style={styles.title}>
+                      {allCountries[randomInts[index]]['name']['common']}
+                    </Text>
+                  </TouchableOpacity>
+                );
+              }
+            })}
+          <TouchableOpacity style={styles.button}>
+            <Text style={[styles.title, styles.buttonText]}>Continue</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </ScrollView>
   );
 }
 
@@ -40,8 +84,59 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  scrollview: {
+    width: '95%',
+    alignSelf: 'center',
+    flex: 1,
+  },
+  imageWrapper: {
+    flex: 1,
+    marginBottom: 10,
+  },
+  itemsWrapper: {
+    flex: 1,
+  },
   image: {
+    borderRadius: 15,
+    flex: 1,
+    width: '95%',
+    height: '90%',
+    alignSelf: 'center',
+  },
+  icon: {
+    alignSelf: 'flex-end',
+    margin: 15,
+    position: 'relative',
+  },
+  item: {
+    marginVertical: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '95%',
+    borderRadius: 10,
+    alignSelf: 'center',
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: COLOURS.lightGrey,
+    ...SHADOW,
+    padding: 20,
+  },
+  title: {
+    fontSize: 18,
+    textAlign: 'center',
+    fontWeight: '700',
+  },
+  button: {
+    backgroundColor: 'black',
     width: '50%',
-    height: '40%',
+    alignSelf: 'center',
+    marginTop: 10,
+    padding: 15,
+    justifyContent: 'center',
+    borderRadius: 100,
+    ...SHADOW,
+  },
+  buttonText: {
+    color: 'white',
   },
 });
