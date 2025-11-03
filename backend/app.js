ad404378d0ffada18ea671055d573b25177886a6
mayagwright20 <mayagwright20@hotmail.com>
2025-11-03 22:47:41 +0000

    Add Procfile

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import { config } from "dotenv";
 import express from "express";
+import cors from cors;
 
 config({
     path: "./data/config.env"
@@ -7,7 +8,14 @@ config({
 
 export const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(
+    cors({
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        origin: [process.env.FRONTEND_URI_1, process.env.FRONTEND_URI_2]
+    })
+)
 
 app.get("/", (req, res, nex)=> {
     res.send("Working")
