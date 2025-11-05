de3b25eca86f939eef09e97be0a558d30b6eac68
mayagwright20 <mayagwright20@hotmail.com>
2025-11-05 13:39:47 +0000

    Add correct basepath

--- server/server.js
+++
@@ -1,8 +0,0 @@
-import { app } from "./app.js";
-import { connectDB } from "./data/database.js";
-
-connectDB();
-
-app.listen(process.env.PORT, () => {
-    console.log(`Listening on PORT ${process.env.PORT}`)
-});
